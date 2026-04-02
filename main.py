8eb694e67a5eeb8975ac5599c33f1a36c6ab5199
Divyansh <yadavdivyansh445@gmail.com>
2026-04-02 23:13:58 +0530

    ranker algorithm

--- main.py
+++ main.py
@@ -229,5 +229,76 @@ async def match_resume(request:MatchRequest):
     
 
 
+# ============================
+# RANKING ENDPOINT
+# ============================
         
+"""step -1 to fetch the jd text we have id as jd_text that is returned
+by generate jd function we can store the jd text in the ranker algo
+
+step - 2 for every candidatefetch the file name and resumetext from the db 
+
+
+step -3 for each candidate calculate the score and return the
+matched skills,missing skills,score add it to the list 
+
+step - 4 sort these candidates by score 
+step - 5 make a get endpoint that will get all the details
+click the match all candidates button and it will render the
+results in the ui
+
+
+now what my optimized solution is that i already calculated 
+the skills for each candidate and stored them in the db
+
+so scores and matching and missing skills are calculated 
+by skill matcher function in this route again if they need 
+to otherwise they are calculated in the match skills endpoint 
+
+after this step store them in a candidate list make a get endpoint
+and render it in the ui after clicking the match all button
+"""
+@app.get("/match-all")
+async def match_all():
+   try: 
+        conn = sqlite3.connect('hiring.db')
+        cursor = conn.cursor()
+
+        cursor.execute("SELECT jd_text " \
+        "FROM job_descriptions " \
+        "ORDER BY id DESC " \
+        "LIMIT 1")
+        row = cursor.fetchone()
+        if not row:
+            conn.close()
+            return{"error":"jd not found"}
+        jd_text = row[0]
+
+        cursor.execute("SELECT filename, skills FROM candidates")
+        candidates= cursor.fetchall()
+        if not row:
+            conn.close()
+            return{"error":"candidates skills not found"}
+        
+        jd_skills = set(extract_skills(jd_text))
+        candidate_results = []
+        for filename, skills in candidates:
+            candidate_skills = set(json.loads(skills))
+            matched = jd_skills & candidate_skills
+            missing = jd_skills - candidate_skills
+
+            score = len(matched) / len(jd_skills)
+
+            candidate_results.append({
+                "filename": filename,
+                "score": score,
+                "matched_skills": list(matched),
+                "missing_skills": list(missing)
+            })
+        candidate_results.sort(key =lambda x:x["score"],reverse=True)
+        conn.close()
+        return{"results":candidate_results}
+   except Exception as e:
+        return{"error":str(e)}
     
+       
